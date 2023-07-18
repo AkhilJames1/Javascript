9496c97ec96cfc8d15181733f458c696cd68be47
Akhil <akhil.j@neoito.com>
2023-07-18 07:59:05 +0530

    6.1 recursion

---
+++ tasks/Study/Study.js
@@ -0,0 +1,131 @@
+// array sort
+// convert negative to positive
+// remove negatives
+// array reverse
+// count elements in array
+// add element to (first,last) position
+// remove elements from first,last
+// find specific elemnt from array(array.find or for loop)
+// join 2 arrays (concat method)
+// map ,foreach ,sort, filter, find,
+// array inbuilt methods 
+//undefined and null difference
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+
+console.clear()
+console.log('hi')
+
+// let arr = [7,2,2,9,4,-2,3,2,9,6,4]
+// let strArry = ["Akhil", "Tony", "Chris", "Thomas", "Sony", "Tarson"]
+
+
+// arr= arr.concat(strArry)
+// let users =  {
+//     name: "Akhil",
+//     id: 1
+// }
+// let users2 =  {
+//     name: "Amal",
+//     id: 2
+// }
+
+// let numOfElemtnts = (sumofnum)=> 
+// {
+ 
+//     sumofnum.unshift('Akhil', "Caper")
+//     sumofnum.pop()
+//     sumofnum.shift()
+//     return sumofnum;
+   
+    
+    
+
+// }
+// console.log(numOfElemtnts(strArry))
+
+
+// let testing  = (element)=> {
+   
+//     const value = 0;
+//     return value;
+
+    
+// }
+
+// // let startingWithT = (element)=> {
+// //    return "Hello"
+    
+
+// // }
+
+// let convertToNegative = (value)=> {
+//     let arr2 = [];
+//     for(let item of value)
+//     {
+      
+        
+//         if(item<0)
+//         {
+//            arr2.push(-item);
+//         }
+//         else {
+//             arr2.push(item)
+//         }
+        
+       
+        
+//     }
+//     return arr2;
+
+
+
+// }
+// console.log(convertToNegative(arr))
+
+
+
+
+const newName = ["Akhil", "Amal", "Chris"];
+const nameswithHtml = [];
+
+    for(let name of newName)
+    {
+        nameswithHtml.push(`${name}`)
+    }
+
+    console.log(nameswithHtml)
+
+
+
+
+
+
+
+// console.log(startingWithT(strArry))
+
+
+// console.log(testing(strArry))
+
+    
+    
+
+
+    // for(let item of arr)
+    // {
+    //     length = arr.length()
+    //     for(let i=1, i<=8 i++ )
+    // }
+
+
+
+    // const sortedValue = u1.sort((a,b)=>
+    // {
+    //     return b-a
+
+    // })
+    // console.log(sortedValue)
+
+
